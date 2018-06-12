60bdf43c036b979877184940772e02097e33654d
KatlheenEscoto <31053437+KatlheenEscoto@users.noreply.github.com>
2018-06-12 05:22:06 +0200

    Agregando el codigo que Walter dijo en cada actividad xd

--- EngBook/app/src/main/java/com/example/hp/engbook/vistas/IntentoExamenActivity.java
+++ EngBook/app/src/main/java/com/example/hp/engbook/vistas/IntentoExamenActivity.java
@@ -1,12 +1,16 @@
 package com.example.hp.engbook.vistas;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.hp.engbook.DataBase.DataBase;
+import com.example.hp.engbook.Metodos;
 import com.example.hp.engbook.R;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
@@ -55,7 +59,7 @@ public class IntentoExamenActivity extends AppCompatActivity {
             set1.setFillAlpha(110);
             set1.setColor(Color.RED);
             set1.setLineWidth(2);
-            set1.setValueTextSize(10f);
+            set1.setValueTextSize(15f);
             set1.setCircleColor(Color.BLACK);
 
             ArrayList<ILineDataSet> dataSets = new ArrayList<>();
@@ -66,8 +70,35 @@ public class IntentoExamenActivity extends AppCompatActivity {
             mChart.setData(data);
 
         }else{
-            Toast.makeText(this, "No existen datos", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Usted NO ha realizado el examen.", Toast.LENGTH_LONG).show();
         }
 
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        getMenuInflater().inflate(R.menu.menu_salir,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.opcion:
+                salir();
+                break;
+            case R.id.opcion2:
+                Intent i = new Intent(this, DondeEstoyActivity.class);
+                startActivity(i);
+                break;
+        }
+        return true;
+    }
+
+    private void salir() {
+        Metodos.saveFileS(this,"log");
+        System.exit(0);
     }
 }
--- EngBook/app/src/main/java/com/example/hp/engbook/vistas/NivelExamenActivity.java
+++ EngBook/app/src/main/java/com/example/hp/engbook/vistas/NivelExamenActivity.java
@@ -1,12 +1,16 @@
 package com.example.hp.engbook.vistas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.hp.engbook.DataBase.DataBase;
+import com.example.hp.engbook.Metodos;
 import com.example.hp.engbook.R;
 import com.example.hp.engbook.model.Intento_Examen;
 
@@ -62,5 +66,32 @@ public class NivelExamenActivity extends AppCompatActivity {
 
     public void limpiarTexto(View v){
         edtPuntaje.setText("");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        getMenuInflater().inflate(R.menu.menu_salir,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.opcion:
+                salir();
+                break;
+            case R.id.opcion2:
+                Intent i = new Intent(this, DondeEstoyActivity.class);
+                startActivity(i);
+                break;
+        }
+        return true;
+    }
+
+    private void salir() {
+        Metodos.saveFileS(this,"log");
+        System.exit(0);
     }
 }
